1bbabda866f664d9dd7f8817dcf0e36357acd970
RomanKuzhekov <profet@inbox.ru>
2022-10-09 22:58:05 +0300

    Установил русский язык в конфиге

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -10,6 +10,7 @@ return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'ru-RU',
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
         'request' => [
