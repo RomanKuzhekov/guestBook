844b576ed361a67998885f7eacab9a41b4d161dc
RomanKuzhekov <profet@inbox.ru>
2022-10-10 15:53:33 +0300

    Создал форму и миграцию для гостевой книги

---
+++ frontend/models/Guestbook.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace frontend\models;
+
+use yii\db\ActiveRecord;
+
+class Guestbook extends ActiveRecord
+{
+
+    public static function tableName()
+    {
+        return 'guestbook';
+    }
+
+    public function rules()
+    {
+        return [
+            [['name', 'text'], 'required', 'message' => 'Заполните поле'],
+            [['name'], 'string', 'max' => 100],
+            [['name', 'text'], 'trim'],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Имя',
+            'text' => 'Текст',
+        ];
+    }
+}
